e31c27b33e76ed63e1d58d168fc2e18334c35d00
mardanniel <60316252+mardanniel@users.noreply.github.com>
2022-11-03 00:45:42 +0800

    update storage

--- src/services/storage.tsx
+++ src/services/storage.tsx
@@ -2,30 +2,22 @@ import { Schedule } from "../data/interfaces/schedule"
 
 const DEFAULT_KEY = 'schedules';
 
-export const createSchedule = async (schedule: Schedule): Promise<void> => {
-  try {
-    let schedules: Schedule[] =  await getSchedules();
-    schedules.push(schedule);
-    new Promise((resolve, reject) => {
-      chrome.storage.local.set({ [DEFAULT_KEY]: schedules }, () => {           
-        if (chrome.runtime.lastError)
-          reject(chrome.runtime.lastError);
-        resolve('Successfully stored schedule!');
-      });
-    });
-  }catch(e){
-    console.log(e);
-  }
+export const createSchedule = async (schedule: Schedule): Promise<string> => {
+  let schedules: Schedule[] =  await getSchedules();
+  schedules.push(schedule);
+  return chrome.storage.local.set({ 
+    [DEFAULT_KEY]: schedules 
+  })
+  .then((value) => value)
+  .catch((error) => error)
 }
 
 export const getSchedules = async (): Promise<Schedule[]> => {
   return new Promise((resolve, reject) => {
     chrome.storage.local.get([DEFAULT_KEY], (result) => {
-      if(chrome.runtime.lastError){
-        reject(chrome.runtime.lastError);
-      }else{
-        resolve(result.schedules ?? []);
-      }
+      chrome.runtime.lastError
+      ? reject(chrome.runtime.lastError)
+      : resolve(result.schedules ?? []);
     });
   })
   .then((value) => {
@@ -35,11 +27,8 @@ export const getSchedules = async (): Promise<Schedule[]> => {
   .catch((error) => error);
 }
 
-export const clearSchedules = async (): Promise<string> => {
-  return new Promise((resolve, reject) => {
-    chrome.storage.local.clear();
-    if(chrome.runtime.lastError)
-          reject(chrome.runtime.lastError);
-    resolve("Successfully cleared schedules!");
-  });
+export const clearSchedules = async (): Promise<boolean | string> => {
+  return chrome.storage.local.clear()
+  .then(() => true)
+  .catch((error) => error)
 }
