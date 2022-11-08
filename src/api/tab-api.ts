cb3bf671533e8210c48d87283d8b202409e822b0
mardanniel <60316252+mardanniel@users.noreply.github.com>
2022-11-09 00:47:18 +0800

    refactor, lint

--- src/api/tab-api.ts
+++ src/api/tab-api.ts
@@ -1,7 +1,7 @@
 export const openInNewTab = (createTabOptions: chrome.tabs.CreateProperties) => {
   new Promise((resolve) => {
     chrome.tabs.create(createTabOptions, (tab) => {
-      console.log(`Successfully created ${tab.id}`)
-    })
-  })
-}
+      resolve(`Successfully created ${tab.id}`);
+    });
+  });
+};
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -9,16 +9,16 @@ const DATE_OPTIONS: Intl.DateTimeFormatOptions = {
 };
 
 export const getLocaleDateStringFromLong = (date: number): string => {
-  return new Date(date).toLocaleDateString("en-US", DATE_OPTIONS);
-}
+  return new Date(date).toLocaleDateString('en-US', DATE_OPTIONS);
+};
 
 export const getISOStringFromLong = (date: number): string => {
-  let dateVar = new Date(date), pad = (num: number) => (num < 10 ? '0' : '') + num;
-  return `${dateVar.getFullYear()}-${pad(dateVar.getMonth() + 1)}-${pad(dateVar.getDate())}T${pad(dateVar.getHours())}:${pad(dateVar.getMinutes())}`
-}
+  const dateVar = new Date(date), pad = (num: number) => (num < 10 ? '0' : '') + num;
+  return `${dateVar.getFullYear()}-${pad(dateVar.getMonth() + 1)}-${pad(dateVar.getDate())}T${pad(dateVar.getHours())}:${pad(dateVar.getMinutes())}`;
+};
 
 export const parseDateToLong = (dateString: string): number => {
   return Date.parse(dateString);
-}
+};
 
 
